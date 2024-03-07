ad5ca446c3888f063e297da493117b871205d425
cesartalen <cesartalense@gmail.com>
2024-03-07 23:42:17 +0100

    userSchema, register user and login user with JWT

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,15 +1,58 @@
+import bcrypt from 'bcryptjs'
+import jsonwebtoken from 'jsonwebtoken'
 import { AppError } from '../utils/appError.js'
 import { catchAsync } from '../utils/catchAsync.js'
+import { User } from '../models/userSchema.js'
+
+const { sign, decode, verify } = jsonwebtoken
 
 export const createUser = catchAsync(async (req, res, next) => {
-  const { name, email, password, passwordConfirm } = req.body
+  const { name, email, password } = req.body
 
-  if (!name || !email || !password || !passwordConfirm) {
+  if (!name || !email || !password) {
     return next(new AppError('All fields must be filled!', 400))
   }
-  res.send('register user')
+
+  const userExistsEmail = await User.findOne({ email })
+  const userExistsName = await User.findOne({ name })
+
+  if (userExistsEmail || userExistsName) {
+    return next(new AppError('User already exists!', 400))
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  })
+
+  await user.save()
+
+  res.status(201).json({ status: true, name: user.name })
 })
 
+const generateToken = (id) => {
+  return jsonwebtoken.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '14d',
+  })
+}
+
 export const loginUser = catchAsync(async (req, res) => {
-  res.send('login user')
+  const { name, password } = req.body
+
+  const user = await User.findOne({ name })
+
+  if (user && (await bcrypt.compare(password, user.password))) {
+    res.status(200).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    })
+  } else {
+    return next(new AppError('Invalid login!', 401))
+  }
 })
---
+++ backend/models/userSchema.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A username is required!'],
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'An email is required!'],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, 'Please provide a password'],
+      minlength: 8,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+export const User = mongoose.model('User', userSchema)
