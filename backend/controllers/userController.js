ccfaec6ec0868264b62876b680a90cfaa311d967
cesartalen <cesartalense@gmail.com>
2024-03-07 16:17:30 +0100

    catchAsync util function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,15 @@
 import { AppError } from '../utils/appError.js'
+import { catchAsync } from '../utils/catchAsync.js'
 
-export const createUser = (req, res, next) => {
+export const createUser = catchAsync(async (req, res, next) => {
   const { name, email, password, passwordConfirm } = req.body
 
   if (!name || !email || !password || !passwordConfirm) {
     return next(new AppError('All fields must be filled!', 400))
   }
   res.send('register user')
-}
+})
 
-export const loginUser = (req, res) => {
+export const loginUser = catchAsync(async (req, res) => {
   res.send('login user')
-}
+})
