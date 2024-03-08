8727725b28cbffd8543fb35639f6d2f0e11a0bfe
cesartalen <cesartalense@gmail.com>
2024-03-08 11:53:58 +0100

    basic pages, routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,26 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import RegisterPage from './pages/RegisterPage'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>
+  },
+  {
+    path: "/login",
+    element: <LoginPage/>
+  },
+  {
+    path: "/register",
+    element: <RegisterPage/>
+  }
+])
+
 function App() {
   return (
-    <>
-    </>
+    <RouterProvider router={router}/>
   )
 }
 
