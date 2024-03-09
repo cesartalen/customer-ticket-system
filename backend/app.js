f4a0fc679a85be9d759ae88e050425ba0312163b
cesartalen <cesartalense@gmail.com>
2024-03-09 16:12:52 +0100

    app.use cors

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,18 @@
 import express from 'express'
+import cors from 'cors'
 import { router as userRouter } from './routes/userRouter.js'
 
 const app = express()
 
 app.use(express.json())
+
+app.use(
+  cors({
+    origin: ['http://localhost:5173', 'http://www.localhost:5173'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+)
 
 app.get('/', (req, res) => {
   res.json({ message: 'Test' })
