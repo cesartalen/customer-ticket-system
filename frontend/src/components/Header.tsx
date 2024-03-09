3e2087db834f73d2471101f79087372879df3afc
cesartalen <cesartalense@gmail.com>
2024-03-09 13:58:41 +0100

    header showing (login register)/logout based on login status

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,12 @@
 import { AiFillHome, AiOutlineLogin, AiOutlineLogout, AiOutlineUserAdd, AiFillQuestionCircle} from 'react-icons/ai'
 import { Link } from 'react-router-dom'
+import { useStore } from 'zustand'
+import { useUserState } from '../store/userState'     
+
 
 export const Header = () => {
+  const userState = useStore(useUserState)
+  console.log(userState.status)
   return (
     <>
       <div className='header'>
@@ -15,16 +20,26 @@ export const Header = () => {
                 <AiFillQuestionCircle/> FAQ
               </Link>
             </li>
+            {userState.status ? (
             <li>
               <Link to='/Login'>
-                <AiOutlineLogin/> Login
+                <AiOutlineLogout/> Logout
               </Link>
             </li>
-            <li>
-              <Link to='/Register'>
-                <AiOutlineUserAdd/> Register
-              </Link>
-            </li>
+            ) : (
+              <>
+                <li>
+                  <Link to='/Login'>
+                    <AiOutlineLogin/> Login
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/Register'>
+                    <AiOutlineUserAdd/> Register
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
         </div>
       </div>
