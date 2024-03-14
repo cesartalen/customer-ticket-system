09e3159478b0efb8e1a14bbe686e5dc6c49b0548
cesartalen <cesartalense@gmail.com>
2024-03-14 15:33:22 +0100

    faq page layout, design

--- frontend/src/pages/FAQPage.tsx
+++ frontend/src/pages/FAQPage.tsx
@@ -1,5 +1,24 @@
 export default function FAQPage() {
   return(
-    <div>Read these before submitting a ticket!</div>
+    <div className='faq-contents'>
+      <div className='faq-label'>
+        <a className='g-title'>Frequently asked questions</a>
+        <a className='g-subtitle'>Read these before submitting your own ticket!</a>
+      </div>
+      <div className='faq-items'>
+        <div className='faq-item'>
+          <a className='faq-question'>Can I...</a>
+          <a className='faq-answer'>Yes, head over to...</a>
+        </div>
+        <div className='faq-item'>
+          <a className='faq-question'>Question 2</a>
+          <a className='faq-answer'>Answer</a>
+        </div>
+        <div className='faq-item'>
+          <a className='faq-question'>Question 3</a>
+          <a className='faq-answer'>Answer</a>
+        </div>
+      </div>
+    </div>
   )
 }
