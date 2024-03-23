89ab6df6512451d5bc6331bfcd754588df91109b
cesartalen <cesartalense@gmail.com>
2024-03-23 23:19:24 +0100

    username and email regex added to schema. min and maxlengths added added to email, name and password

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -7,17 +7,23 @@ const userSchema = new mongoose.Schema(
       required: [true, 'A username is required!'],
       unique: true,
       lowercase: true,
+      maxlength: 32,
+      minlength: 3,
+      match: /^[A-Za-z0-9]+$/,
     },
     email: {
       type: String,
       required: [true, 'An email is required!'],
       unique: true,
       lowercase: true,
+      maxlength: 64,
+      match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
     },
     password: {
       type: String,
       required: [true, 'Please provide a password'],
       minlength: 8,
+      maxlength: 32,
     },
     isAdmin: {
       type: Boolean,
